99931c8fc414670a316e1f94e0f597f7be7fa448
Jacob Sapoznikow <jacob1coder@gmail.com>
2024-02-08 18:38:16 +0000

    mut

--- src/codegen/rust.rs
+++ src/codegen/rust.rs
@@ -32,7 +32,7 @@ pub fn gen_function(
     gen: &Generator,
     class: &ClassExpr,
     FunctionExpr {
-        is_mut: _,
+        is_mut,
         is_static,
         name,
         source,
@@ -49,7 +49,7 @@ pub fn gen_function(
     let src = source.unwrap_or(*class.name.clone()).ident()?;
     let cname = class.name.ident()?;
     // let ret = ret.unwrap_or(Expr::Identifier("()".to_string())).ident()?;
-    
+
     let mut args = Vec::new();
     let mut args_names = Vec::new();
 
@@ -61,16 +61,23 @@ pub fn gen_function(
     let args = args.join(",\n    ");
     let args_names = args_names.join(", ");
 
-    let args = if args.len() > 0 { format!(",\n    {}", args) } else { args };
-    let args_names = if args_names.len() > 0 && !is_static { format!(", {}", args_names) } else { args_names };
+    let args = if args.len() > 0 {
+        format!(",\n    {}", args)
+    } else {
+        args
+    };
+    let args_names = if args_names.len() > 0 && !is_static {
+        format!(", {}", args_names)
+    } else {
+        args_names
+    };
 
-    let function_head =
-"#[no_mangle]
+    let function_head = "#[no_mangle]
 #[allow(unused_mut, unused_variables, unused_unsafe, non_snake_case, improper_ctypes_definitions)]";
 
     let code = if is_static {
         format!(
-"{function_head}
+            "{function_head}
 pub unsafe extern \"system\" fn {fn_name}<'local>(
     mut env: *mut jni::JNIEnv<'local>,
     class: jni::objects::JClass<'local>{args}
@@ -86,8 +93,29 @@ pub unsafe extern \"system\" fn {fn_name}<'local>(
             raw_cname = raw_cname,
         )
     } else {
-        format!(
-"{function_head}
+        if is_mut {
+            format!(
+                "{function_head}
+pub unsafe extern \"system\" fn {fn_name}<'local>(
+    mut env: *mut jni::JNIEnv<'local>,
+    class: jni::objects::JClass<'local>,
+    this: jlong{args}
+) -> jobject {{
+    let this: &mut {cname} = jlong_to_pointer::<{cname}>(this).as_mut().unwrap();
+    object_to_jobject(env, {src}::{rust_fn_name}(this{args_names}), \"{raw_cname}\".to_string())
+}}",
+                fn_name = fn_name,
+                cname = cname,
+                rust_fn_name = rust_fn_name,
+                function_head = function_head,
+                src = src,
+                args = args,
+                args_names = args_names,
+                raw_cname = raw_cname,
+            )
+        } else {
+            format!(
+                "{function_head}
 pub unsafe extern \"system\" fn {fn_name}<'local>(
     mut env: *mut jni::JNIEnv<'local>,
     class: jni::objects::JClass<'local>,
@@ -96,15 +124,16 @@ pub unsafe extern \"system\" fn {fn_name}<'local>(
     let this: &{cname} = jlong_to_pointer::<{cname}>(this).as_mut().unwrap();
     object_to_jobject(env, {src}::{rust_fn_name}(this{args_names}), \"{raw_cname}\".to_string())
 }}",
-            fn_name = fn_name,
-            cname = cname,
-            rust_fn_name = rust_fn_name,
-            function_head = function_head,
-            src = src,
-            args = args,
-            args_names = args_names,
-            raw_cname = raw_cname,
-        )
+                fn_name = fn_name,
+                cname = cname,
+                rust_fn_name = rust_fn_name,
+                function_head = function_head,
+                src = src,
+                args = args,
+                args_names = args_names,
+                raw_cname = raw_cname,
+            )
+        }
     };
 
     Ok(code)
